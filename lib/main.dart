4a51482a8dcafd4e1432bf8eb2d7ef3448750cc0
ASHISH-11112004 <ashish2004bkp@gmail.com>
2024-10-17 20:46:04 +0530

    Your descriptive commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -43,96 +43,97 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Colors.purple,
           title: const Text('Calculator_App'),
         ),
-        body:Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 70),
-                child: FaIcon(FontAwesomeIcons.calculator,size: 50,),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: TextField(
-                  decoration: InputDecoration(
+        body:SingleChildScrollView(
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 70),
+                  child: FaIcon(FontAwesomeIcons.calculator,size: 50,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.pink)
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.black,width: 15)
+                        )
+                    ),
+                    keyboardType: TextInputType.number,
+                    controller: no1controller,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: TextField(
+                    decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.pink)
+                        borderSide: BorderSide(color: Colors.pink)
                       ),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(color: Colors.black,width: 15)
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.black,width: 15)
                       )
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: no1controller,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pink)
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(color: Colors.black,width: 15)
-                    )
+                    keyboardType: TextInputType.number,
+                    controller: no2controller,
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: no2controller,
                 ),
-              ),
-              Container(height: 50,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(onPressed: (){
-                    var no1 = int.parse(no1controller.text.toString());
-                    var no2 = int.parse(no2controller.text.toString());
-                    var sum=no1+no2;
-                    result = "The sum of $no1 and $no2 is $sum";
-                    setState(() {
-                    });
-                  }, child:Text('Add')),
-                  ElevatedButton(onPressed: (){
-                    var no1 = int.parse(no1controller.text.toString());
-                    var no2 = int.parse(no2controller.text.toString());
-                    var sub=no1-no2;
-                    result = "The subtraction of $no1 and $no2 is $sub";
-                    setState(() {
-                    });
+                Container(height: 50,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1controller.text.toString());
+                      var no2 = int.parse(no2controller.text.toString());
+                      var sum=no1+no2;
+                      result = "The sum of $no1 and $no2 is $sum";
+                      setState(() {
+                      });
+                    }, child:Text('Add')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1controller.text.toString());
+                      var no2 = int.parse(no2controller.text.toString());
+                      var sub=no1-no2;
+                      result = "The subtraction of $no1 and $no2 is $sub";
+                      setState(() {
+                      });
 
-                  }, child:Text('Subtract')),
-                  ElevatedButton(onPressed: (){
-                    var no1 = int.parse(no1controller.text.toString());
-                    var no2 = int.parse(no2controller.text.toString());
-                    var multi=no1*no2;
-                    result = "The multiplication of $no1 and $no2 is $multi";
-                    setState(() {
-                    });
+                    }, child:Text('Subtract')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1controller.text.toString());
+                      var no2 = int.parse(no2controller.text.toString());
+                      var multi=no1*no2;
+                      result = "The multiplication of $no1 and $no2 is $multi";
+                      setState(() {
+                      });
 
-                  }, child:Text('Multiply')),
-                  ElevatedButton(onPressed: (){
-                    var no1 = int.parse(no1controller.text.toString());
-                    var no2 = int.parse(no2controller.text.toString());
-                    var div=no1/no2;
-                    result = "The division of $no1 and $no2 is $div";
-                    setState(() {
-                    });
+                    }, child:Text('Multiply')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1controller.text.toString());
+                      var no2 = int.parse(no2controller.text.toString());
+                      var div=no1/no2;
+                      result = "The division of $no1 and $no2 is $div";
+                      setState(() {
+                      });
 
-                  }, child:Text('Division')),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(result,style: TextStyle(fontSize: 30,color: Colors.black,),),
-              ),
-
-            ],
+                    }, child:Text('Division')),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child:Text(result,style: TextStyle(fontSize: 30,color: Colors.black,)),),
+              ],
+            ),
           ),
         )
 
